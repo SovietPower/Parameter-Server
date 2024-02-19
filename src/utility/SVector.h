6ca263c9f95d29c6a9438044a2ba46670ec17b92
SovietPower <sovietpower@foxmail.com>
2024-02-19 23:46:31 +0800

    fix: 修改deleter相关行为，避免内存泄露

--- src/utility/SVector.h
+++ src/utility/SVector.h
@@ -33,17 +33,18 @@ constexpr bool IsRelocatableV = IsRelocatable<T>::value;
 AddRelocatableT(std::vector)
 // AddRelocatable(std::string) // 部分SSO实现可能导致它不是
 
-#undef AddRelocatable
-#undef AddRelocatableT
+// #undef AddRelocatable
+// #undef AddRelocatableT
 
 // --- Deleter
-namespace  {
+namespace {
 /**
  * 因为对象被从 src relocate 到 dst 后，不能再调用 src 的析构，因为这相当于调用 dst 的析构。
  * 此时需要仅释放内存而不调用元素的析构函数。
  * 因此当进行 relocate 时，需要`deallocate(data)`；当不进行 relocate、单纯释放时，需要析构元素再释放。
  * 即传给 shared_ptr 的 deleter 必须有状态，或可改变，以根据情况执行不同语句。
  * 但 sptr 创建后不能改变 deleter，故只能为其附加状态：用一个 enum 值表示是否要析构元素，然后在需要时通过 get_deleter 修改。
+ * 注意 data [size, capacity) 的部分元素尚未进行构造，因此只能析构 [0, size)；但释放内存时需要传入 capacity。因此两者都要保存。
  */
 
 /**
@@ -93,20 +94,20 @@ struct Deleter {
  * 使用引用计数保证底层的 data 被正确释放、不会出现悬垂指针。
  * 与 shared_ptr 类似，SVector 的非 const 操作不是线程安全的。
 
- * 注：为了减少接口数量，allocator 默认进行值初始化，且只有最基本的构造函数支持设置 allocator。
- * 可以先设置后，再调用其它逻辑实现其它的构造方式。
+ * 注：为了减少接口数量，allocator 默认进行值初始化，且只有最基本的构造函数支持设置 allocator。可以先设置后，再调用其它逻辑实现其它的构造方式。
+ * 因此由于共享的复杂性，不能与 vector 行为一致：整个 data [0, capacity) 的位置的元素都要完成构造，即使没有使用。这样释放时不需要已知 size，只需要 capacity。
  */
 
 /**
- * @brief 共享数组
+ * @brief 可共享的 Vector
  * @tparam T 元素的类型
  */
 template <typename T, typename Allocator = std::allocator<T>>
 	requires
 		requires (Allocator a, size_t n, T* p) {
-			a.allocate(n);
+			// Allocator 需满足分配器 (Allocator) 要求
+			{ a.allocate(n) } -> std::same_as<T*>;
 			a.deallocate(p, n);
-			std::is_same_v<decltype(a.allocate(n)), T*>;
 		}
 class SVector {
 	using DeleterT = Deleter<T, Allocator>;
@@ -115,7 +116,7 @@ class SVector {
 	SVector(): size_(0), capacity_(0), ptr_(nullptr), allocator() {}
 
 	/** @brief 由 shared_ptr<T> 的 deleter 决定何时释放数组 */
-	~SVector() {}
+	~SVector() = default;
 
 	/**
 	 * @brief 构造拥有 count 个值 value 的元素的 SVector。
@@ -136,10 +137,10 @@ class SVector {
 	 * @brief 通过 SVector 构造，与其共享底层数组和引用计数，无需拷贝。
 	 * @param other 另一个 SVector。~~不需要与其有相同的模板类型，但 U* 必须可转换为 T*~~
 	 */
-	explicit SVector(const SVector& other) {
+	SVector(const SVector& other) {
 		*this = other;
 	}
-	explicit SVector(SVector&& other) {
+	SVector(SVector&& other) {
 		*this = std::move(other);
 	}
 
@@ -173,8 +174,8 @@ class SVector {
 
 	/**
 	 * @brief 通过数组构造，直接将它作为 SVector 的底层数组，无需拷贝。
-	 * 当 vector 非 const 时，可以用它的 data 调用此构造函数，以零拷贝使用 vector：SVector(v.data(), v.capacity(), false)。
-	 * （注意此时 deletable 必须为 false，且需要管理好 vector 的生命期）
+	 * 当 vector 非 const 时，可以用它的 data 调用此构造函数，以零拷贝使用 vector：SVector(v.data(), v.capacity(), false)。但要注意生命周期及 v.data() 的有效性，且 deletable 必须为 false。
+	 * 这样构造的 SVector 不应该发生容量更改。否则 reserve 将可能把原数组中的元素移动到新位置。
 	 * @param data 数组指针
 	 * @param size 数组长度
 	 * @param deletable 当引用计数变为0时，是否要进行析构并释放该数组。应该始终为 false，除非使用同样的 allocator 分配。
@@ -189,20 +190,20 @@ class SVector {
 
 	/**
 	 * @brief 通过 vector 构造，拷贝其数据。
-	 * 当 vector 非 const 时，可调用 SVector(v.data(), v.capacity(), false) 零拷贝获取其数据，但要注意生命周期及 v.data() 的有效性。
+	 * 当 vector 非 const 时，可调用 SVector(v.data(), v.capacity(), false) 零拷贝获取其数据。但要注意生命周期及 v.data() 的有效性。
 	 */
 	explicit SVector(const std::vector<T>& vec): allocator() {
 		CopyFrom(vec.data(), vec.size());
 	}
 
 	/**
-	 * @brief 通过 shared_ptr<vector> 构造，与其共享底层数组和引用计数，无需拷贝。
+	 * @brief 通过 shared_ptr<vector> 构造，与其共享底层数组和引用计数，无需拷贝。但要注意尽管 vector 的生命周期与 SVector 一致，仍要保证 v.data() 的有效性。
 	 */
-	explicit SVector(const std::shared_ptr<std::vector<T>>& other): allocator() {
-		size_ = other->size();
-		capacity_ = other->capacity();
+	explicit SVector(const std::shared_ptr<std::vector<T>>& sp): allocator() {
+		size_ = sp->size();
+		capacity_ = sp->capacity();
 		// shared_ptr 的别名构造函数
-		ptr_ = std::shared_ptr<T>(other, other->data());
+		ptr_ = std::shared_ptr<T>(sp, sp->data());
 	}
 
 	/**
@@ -215,16 +216,18 @@ class SVector {
 	// --- 从其它对象复制数据
 	/**
 	 * @brief 从 SVector 拷贝数据。创建新的引用计数。
+	 * 将拷贝整个底层数组（包括 [size, capacity) 的部分）。
 	 */
 	void CopyFrom(const SVector<T>& other) {
 		if (this == &other) {
 			return;
 		}
-		CopyFrom(other.data(), other.size());
+		CopyFrom(other.data(), other.capacity());
+		size_ = other.size_;
 	}
 
 	/**
-	 * @brief 从数组拷贝数据。创建新的引用计数。
+	 * @brief 从数组拷贝数据。创建新的引用计数。更新 size_、capacity_ 为 size。
 	 */
 	void CopyFrom(const T* src, size_t size) {
 		T* newData = Allocate(size);
@@ -259,6 +262,7 @@ class SVector {
 
 	/**
 	 * @brief 获取底层数组的切片 [left, right)，持有引用计数（类似 go 的切片）。
+	 * 对切片的读写和修改是安全的。当容量改变时会创建新的底层数组，与原数组断开联系。
 	 * @return SVector<T> 包含切片的 SVector
 	 */
 	SVector<T> Slice(size_t left, size_t right) {
@@ -297,102 +301,83 @@ class SVector {
 	}
 
 	/**
-	 * @brief 扩大 SVector 的大小以容纳 size 个元素。超过原容量的位置用 defaultV 填充。
-	 * 保证整个 data 数组都是已构建对象。
-	 * 与 vector 不同的是当 size < capacity 时不会缩小容器。
+	 * @brief 更改 SVector 的大小和容量以容纳 size 个元素。超过原容量的位置用 default_value 填充。
+	 * 与 vector 不同的是当 new_size < capacity 时不会缩小容器；当 new_size < size 时不会进行析构。
 	 * @param size 新的大小
 	 */
-	void resize(size_t size, const T& defaultV = T{}) {
-		// 检查容量大小
-		// 即使 size < capacity_，也仅设置 size_，不减小容量
-		if (size > capacity_) {
-			// 分配新空间，并拷贝/移动旧的元素
-			// 会新建一个引用计数存放新的 data，释放旧 data 的计数
-			T* newData = Allocate(size);
-			if constexpr (std::is_trivially_copy_constructible_v<T>) {
-				memcpy(newData, data(), capacity_ * sizeof(T));
-			} else if (ptr_.use_count() == 1) {
-				bool done = false;
-				// 需要 -Wnoclass-memaccess 关闭不可 memcpy 警告
-				if constexpr (IsRelocatableV<T>) {
-					DeleterT* deleter = std::get_deleter<DeleterT>(ptr_);
-					assert(deleter != nullptr);
-					if (deleter->flag != CannotFree) {
-						// relocate
-						// relocatable 的对象在 memcpy 后，src 和 dst 有且只能有一个调用析构。因此只有当前对象唯一持有 data 的所有权时，才可避免执行析构或不恰当的修改，才能进行 relocate。
-						done = true;
-						memcpy(newData, data(), capacity_ * sizeof(T));
+	void resize(size_t new_size, const T& default_value = T{}) {
+		// 即使 new_size < capacity_，也不减小容量，仅设置 size_，且不进行元素析构
+		if (new_size <= capacity_) {
+			size_ = new_size;
+			return;
+		}
+		// 分配新空间，并拷贝/移动旧的元素，包括 [size, capacity) 部分的元（它们已构造）
+		// 会新建一个引用计数存放新的 data，释放旧 data 的计数
+		T* newData = Allocate(new_size);
+		if constexpr (std::is_trivially_copy_constructible_v<T>) {
+			// memcpy 会隐式构造对象
+			memcpy(newData, data(), capacity_ * sizeof(T));
+		} else if (ptr_.use_count() == 1) {
+			bool done = false;
+			// 需要 -Wnoclass-memaccess 关闭不可 memcpy 的警告
+			if constexpr (IsRelocatableV<T>) {
+				DeleterT* deleter = std::get_deleter<DeleterT>(ptr_);
+				assert(deleter != nullptr);
+				if (deleter->flag != CannotFree) {
+					// relocate
+					// relocatable 的对象在 memcpy 后，src 和 dst 有且只能有一个调用析构。因此只有当前对象唯一持有 data 的所有权时，才可避免执行析构或不恰当的修改，才能进行 relocate。
+					done = true;
+					memcpy(newData, data(), capacity_ * sizeof(T));
 
-						// 当进行 relocate 时，需要是否内存，但不能进行元素析构。
-						deleter->flag = CanOnlyFree;
-					}
+					// 进行 relocate 后，需要释放内存，但不能进行元素析构。
+					deleter->flag = CanOnlyFree;
 				}
-				if (!done) {
-					// move or copy
-					// 尝试移动原数组元素（仅在可 noexcept 移动或不可拷贝时使用移动）
-					// 只有当前对象唯一持有 data 的所有权、将在 reset 后释放它时，才可确保能移动对象
-					T* cur = newData;
-					auto first = getMoveIterIfNoexcept(data());
-					auto last = getMoveIterIfNoexcept(data() + capacity_);
-					for (; first != last; ++first, ++cur) {
-						// *cur = *first;
-						std::construct_at(cur, *first);
-					}
-				}
-			} else {
-				// copy
+			}
+			if (!done) {
+				// move or copy
+				// 尝试移动原数组元素（仅在可 noexcept 移动或不可拷贝时使用移动）
+				// 只有当前对象唯一持有 data 的所有权、将在 reset 后释放它时，才可确保能移动对象
 				T* cur = newData;
-				auto first = data();
-				auto last = data() + capacity_;
+				auto first = getMoveIterIfNoexcept(begin());
+				auto last = getMoveIterIfNoexcept(begin() + capacity_);
 				for (; first != last; ++first, ++cur) {
 					std::construct_at(cur, *first);
+					// *cur = *first;
 				}
 			}
-			// 构造和初始化 capacity_ ~ size - 1 的部分
-			T* cur = newData + capacity_;
-			T* last = newData + size;
-			for (; cur != last; ++cur) {
-				std::construct_at(cur, defaultV);
+		} else {
+			// copy
+			T* cur = newData;
+			auto first = begin();
+			auto last = begin() + capacity_;
+			for (; first != last; ++first, ++cur) {
+				std::construct_at(cur, *first);
 			}
-
-			// newData 是 SVector 分配并构造的，因此最后需要进行析构并回收
-			reset(newData, size, NeedDestruct);
 		}
-		size_ = size;
+
+		// 构造和初始化 [capacity, new_size) 的部分
+		T* cur = newData + capacity_;
+		T* last = newData + new_size;
+		for (; cur != last; ++cur) {
+			std::construct_at(cur, default_value);
+		}
+
+		// newData 是 SVector 分配并构造的，因此最后需要进行析构并回收
+		reset(newData, new_size, NeedDestruct);
 	}
 
 	/**
-	 * @brief 扩大 SVector 的大小以容纳 size 个元素。超过原容量的位置用 value 填充。
-	 * 与 vector 不同的是当 size < capacity 时不会缩小容器。
-	 * @param size 新的容量
-	 * @param value 初始值
-	 */
-	// void resize(size_t size, const T& value) {
-	// 	size_t oldSize = size_;
-	// 	resize(size);
-	// 	// 为 oldSize ~ size - 1 位置的元素设初值 value
-	// 	// 仅当 value 不是 T{} 时，才需要这部分
-	// 	if (size > oldSize) {
-	// 		T* cur = data() + oldSize;
-	// 		auto last = end();
-	// 		for (; cur != last; ++cur) {
-	// 			std::construct_at(cur, value);
-	// 		}
-	// 	}
-	// }
-
-	/**
-	 * @brief 重设 SVector 的容量以容纳 size 个元素。不改变 size。
-	 * 新元素将进行值初始化（这与 vector 不同）。
-	 * @param new_cap 新的容量
+	 * @brief 增加 SVector 的容量以容纳 new_cap 个元素。不改变 size。
+	 * 与 vector 不同，新元素会进行构造和值初始化。
+	 * @param new_cap 新的容量。小于等于原容量时不做任何事。
 	 */
 	void reserve(size_t new_cap) {
-		if (new_cap > capacity_) {
-			auto oldSize = size_;
-			resize(new_cap);
-			size_ = oldSize;
+		if (new_cap <= capacity_) {
+			return;
 		}
-		"ovo";
+		auto oldSize = size_;
+		resize(new_cap);
+		size_ = oldSize;
 	}
 
 	template <typename... Args>
@@ -405,17 +390,17 @@ class SVector {
 			}
 		}
 		assert(size_ < capacity_);
-		// 扩容后会进行元素构造，不需要再构造
-		// auto ret = *std::construct_at(end(), std::forward<Args>(args)...);
-		auto ret = (*end() = T(std::forward<Args>(args)...));
 		size_++;
-		return ret;
+		std::destroy_at(end() - 1); // 注意末尾处已有构造好的元素，要进行析构
+		return *std::construct_at(end() - 1, std::forward<Args>(args)...);
+
+		// 如果不使用 construct_at 或 placement new，则会多一次拷贝/移动，不能原地构造
+		// return (*(data() + size_ - 1) = T(std::forward<Args>(args)...)); // 已构造，直接赋值即可
 	}
 
 	void push_back(const T& value) {
 		emplace_back(value);
 	}
-
 	void push_back(T&& value) {
 		emplace_back(std::move(value));
 	}
@@ -424,7 +409,7 @@ class SVector {
 	// void pop_back() noexcept {
 	// 	assert(size_ > 0);
 	// 	size_--;
-	// 	end()->~T(); // 注意，删除元素后可能要析构它
+	// 	end()->~T(); // 注意，删除元素后要析构它
 	// 	// std::destroy_at(end());
 	// }
 
@@ -520,7 +505,7 @@ class SVector {
 	 * @param tab 起始缩进。
 	 * @param lim 元素的输出数量上限。-1 为全部输出。
 	 */
-	std::string DebugString(int lim = 10, int tab = 0) {
+	std::string DebugString(size_t lim = 10, size_t tab = 0) const {
 		std::string tabStr(tab, '\t');
 		std::stringstream ss;
 		#define Output(str) ss << tabStr << str
@@ -528,17 +513,17 @@ class SVector {
 		Output("\tsize: ") << size_ << " capacity: " << capacity_ << '\n';
 		// data
 		Output("\tdata: [ ");
-		T* data = this->data();
+		const T* data = this->data(); // 注意是 const T*
 		if (size_ < 2 * lim) {
-			for (int i = 0; i < size_; ++i) {
+			for (size_t i = 0; i < size_; ++i) {
 				ss << data[i] << ' ';
 			}
 		} else {
-			for (int i = 0; i <lim; ++i) {
+			for (size_t i = 0; i <lim; ++i) {
 				ss << data[i] << ' ';
 			}
 			ss << "... ";
-			for (int i = size_ - lim; i < size_; ++i) {
+			for (size_t i = size_ - lim; i < size_; ++i) {
 				ss << data[i] << ' ';
 			}
 		}
@@ -578,10 +563,30 @@ class SVector {
 	Allocator allocator;
 };
 
-template <typename T>
-std::ostream& operator <<(std::ostream& os, const SVector<T>& arg) {
+template <typename T, typename Allocator>
+std::ostream& operator <<(std::ostream& os, const SVector<T, Allocator>& arg) {
 	os << arg.DebugString();
 	return os;
+}
+
+template <class T, class Alloc>
+	requires
+		requires (T a, T b) {
+			{ a == b } -> std::convertible_to<bool>;
+		}
+bool operator ==(const SVector<T, Alloc>& lhs, const SVector<T, Alloc>& rhs) {
+	if (lhs.size() != rhs.size() || lhs.capacity() != rhs.capacity()) {
+		return false;
+	}
+	auto cur = lhs.begin();
+	auto first = rhs.begin();
+	auto last = rhs.end();
+	for (; first != last; ++cur, ++first) {
+		if (*cur != *first) {
+			return false;
+		}
+	}
+	return true;
 }
 
 } // namespace ps
--- src/utility/test/SVector_test.cpp
+++ src/utility/test/SVector_test.cpp
@@ -1,6 +1,4 @@
 #include <gtest/gtest.h>
-#include <memory>
-#include <vector>
 
 #include "../SVector.h"
 
@@ -12,19 +10,22 @@ constexpr int V = 2024;
 struct A {
 	A(): x(V) {}
 	A(int x): x(x) {}
-	~A() {x = 0;}
+	A(int x, int* desCounter): x(x), desCounter(desCounter) { upd(); }
+	~A() {
+		x = 0;
+		if (!alive) {
+			ADD_FAILURE();
+		}
+		alive = false;
+		if (desCounter) {
+			(*desCounter)--;
+		}
+	}
 
-	A(const A& o) {x = o.x;}
-	A(A&& o) {x = o.x; o.x = 0;}
-	A& operator =(const A& o) {
-		x = o.x;
-		return *this;
-	}
-	A& operator =(A&& o) {
-		x = o.x;
-		o.x = 0;
-		return *this;
-	}
+	A(const A& o): x(o.x), desCounter(o.desCounter) { upd(); }
+	A(A&& o) noexcept: x(o.x), desCounter(o.desCounter) {o.x = 0; upd(); }
+	A& operator =(const A& o) = default;
+	A& operator =(A&& o) noexcept = default;
 
 	bool operator == (const A& o) const {
 		return x == o.x;
@@ -32,8 +33,25 @@ struct A {
 	bool operator == (int value) const {
 		return x == value;
 	}
+	void upd() { if (desCounter) (*desCounter)++; }
 	int x;
+
+private:
+	bool alive{true}; // 检查是否重析构
+	int* desCounter{nullptr}; // 检查析构次数是否正确
 };
+
+std::ostream& operator <<(std::ostream& os, const A& arg) {
+	os << arg.x;
+	return os;
+}
+
+// 如果要验证析构次数是否正确，则定义 counter，并用 AC 构造；最后判 counter 是否为0。
+#define AC(value) A(value, &counter)
+
+namespace ps {
+AddRelocatable(A);
+}
 
 TEST(ConstructorTest, T1) {
 	{
@@ -93,6 +111,31 @@ TEST(ConstructorTest, T2) {
 			Ck(s2);
 		}
 		Ck(s1);
+	}
+	{
+		int counter = 0;
+		{
+			SVector<A> s1(3, AC(0)); // 0 0 0
+			EXPECT_EQ(s1[1], 0);
+			s1.reserve(6);
+			EXPECT_EQ(s1.size(), 3);
+			EXPECT_EQ(s1.capacity(), 6);
+			s1[0] = AC(-1);
+			{
+				SVector<A> s2(s1);
+				EXPECT_EQ(s2[0], -1);
+				s2.emplace_back(99, &counter);
+				EXPECT_EQ(s2.size(), 4);
+				EXPECT_EQ(s2.capacity(), 6);
+				EXPECT_EQ(s2[0], -1);
+				EXPECT_EQ(s2[3], 99);
+				s2[1] = AC(-2);
+			}
+			EXPECT_EQ(s1[1], -2);
+			EXPECT_EQ(s1.size(), 3);
+			EXPECT_EQ(s1.capacity(), 6);
+		}
+		EXPECT_EQ(counter, 0);
 	}
 	// (unsupported) SVector(const SVector<U>&)
 	// // {
@@ -158,8 +201,22 @@ TEST(ConstructorTest, T3) {
 		EXPECT_EQ(s1[2], -5);
 	}
 	{
+		// 与 vector 共享底层存储
+		// 注意，修改 vector size 之外的元素将无法被 vector 所用
 		vector<A> v {1, 2, 3, 4, V};
+		v.reserve(10);
 		SVector<A> s1 (v.data(), v.capacity());
+		s1[1] = -2;
+		EXPECT_EQ(v[1], -2);
+		s1[5] = 5;
+		EXPECT_EQ(s1[5], 5);
+		{
+			SVector<A> s2 (s1);
+			EXPECT_EQ(s2[5], 5);
+			s2[2] = -3;
+		}
+		EXPECT_EQ(s1[2], -3);
+		EXPECT_EQ(v[2], -3);
 	}
 }
 
@@ -182,15 +239,187 @@ TEST(ConstructorTest, T4) {
 				s2[1] = -2;
 			}
 			EXPECT_EQ(s1[1], -2);
+
+			s1.emplace_back(6);
+			EXPECT_EQ(s1.size(), 6);
+			EXPECT_GE(s1.capacity(), 6);
+			EXPECT_EQ(s1[5], 6);
 		}
 		EXPECT_EQ(v[0], 1);
 		EXPECT_EQ(v[1], 2);
+		EXPECT_EQ(v.capacity(), 5);
+	}
+	{
+		using std::shared_ptr;
+		auto sp = std::make_shared<vector<A>>(5, A(V));
+		SVector<A> s1 (sp);
+		EXPECT_EQ(s1.size(), 5);
+		EXPECT_EQ(s1[1], V);
+		s1[1] = 1;
+		EXPECT_EQ(s1[1], 1);
+		EXPECT_EQ((*sp)[1], 1);
+		{
+			SVector<A> s2 (sp);
+			EXPECT_EQ(s2[1], 1);
+			s2[1] = 2;
+			EXPECT_EQ(s2[1], 2);
+		}
+		EXPECT_EQ(s1[1], 2);
+		EXPECT_EQ((*sp)[1], 2);
+
+		sp.reset();
+		EXPECT_EQ(s1[1], 2);
+		EXPECT_EQ(s1[4], V);
+	}
+	{
+		SVector<A> s1 {1, 2, 3, 4, 5, V};
+		EXPECT_EQ(s1.size(), 6);
+		EXPECT_EQ(s1.capacity(), 6);
+		EXPECT_EQ(s1[1], 2);
+		s1.emplace_back(7);
+		EXPECT_EQ(s1.size(), 7);
+		EXPECT_GE(s1.capacity(), 7);
+		EXPECT_EQ(s1[1], 2);
+		EXPECT_EQ(s1[6], 7);
+	}
+	puts("---");
+	{
+		SVector<std::string> s1 {"1", "2", "qqq", "\n\n\t", "55555", "******"};
+		EXPECT_EQ(s1.size(), 6);
+		EXPECT_EQ(s1.capacity(), 6);
+		EXPECT_EQ(s1[1], "2");
+		std::cout << s1 << std::endl;
+
+		s1.emplace_back("\t");
+		EXPECT_EQ(s1.size(), 7);
+		EXPECT_GE(s1.capacity(), 7);
+		EXPECT_EQ(s1[1], "2");
+		EXPECT_EQ(s1[6], "\t");
+		std::cout << s1 << std::endl;
 	}
 }
 
+TEST(CopyFromTest, T) {
+	// SVector
+	{
+		SVector<A> s1 {1, 2, 3, 4, V};
+		s1.reserve(10);
+
+		SVector<A> s2;
+		s2.CopyFrom(s1);
+		EXPECT_EQ(s2.size(), 5);
+		EXPECT_EQ(s2.capacity(), 10);
+		EXPECT_TRUE(s1 == s2);
+	}
+	// data
+	{
+		int* p = new int[4]{1, 2, 3, V};
+		SVector<int> s1;
+		s1.CopyFrom(p, 4);
+		EXPECT_EQ(s1[0], 1);
+		EXPECT_EQ(s1[3], V);
+
+		EXPECT_EQ(p[3], V);
+		delete[] p;
+	}
+	{
+		using std::string;
+		string* p = new string[5] {"1", "ab", "cd", "wwwwwwwww", "\n\n\t"};
+		SVector<string> s1;
+		s1.CopyFrom(p, 5);
+		EXPECT_EQ(s1[1], "ab");
+		EXPECT_EQ(s1[3], "wwwwwwwww");
+		EXPECT_EQ(s1[4], "\n\n\t");
+
+		EXPECT_EQ(p[4], "\n\n\t");
+		delete[] p;
+	}
+	// iterator
+	{
+		using std::string;
+		std::map<int, string> m = {
+			{1, "1"},
+			{233, "qvq"},
+			{999, "wwwwwww"},
+			{-1, "\n\n\t"},
+		};
+		// map 的 value_type 是 pair<const Key, T>
+		using T = std::map<int, string>::value_type;
+		SVector<T> s1;
+		s1.CopyFrom(m.begin(), m.end());
+		EXPECT_EQ(s1.size(), 4);
+		EXPECT_EQ(s1.capacity(), 4);
+		// 注意 map 是有序的
+		T v0 = {-1, "\n\n\t"}; EXPECT_EQ(s1[0], v0);
+		T v1 = {1, "1"}; EXPECT_EQ(s1[1], v1);
+		T v2 = {233, "qvq"}; EXPECT_EQ(s1[2], v2);
+		T v3 = {999, "wwwwwww"}; EXPECT_EQ(s1[3], v3);
+	}
+}
+
+TEST(SliceTest, T) {
+	{
+		SVector<std::string> s1 {"a", "111", "999999"};
+		// SVector<std::string> s1 {"a", "111", "999999"};
+		auto s2 = s1.Slice(0, s1.size());
+		{
+			auto s3 = s2.Slice(1, 2);
+			auto s4 = s3.Slice(0, 1);
+		}
+	}
+	int counter = 0;
+	{
+		SVector<A> s1 {AC(1), AC(2), AC(3), AC(4), AC(V)};
+		std::cout << "testing: " << s1 << std::endl;
+		{
+			auto s2 = s1.Slice(2, 4); // 3, 4
+			EXPECT_EQ(s2[0], 3);
+			EXPECT_EQ(s2[1], 4);
+			s1[2] = AC(-3);
+			EXPECT_EQ(s2[0], -3);
+			s2[1] = AC(-4);
+			EXPECT_EQ(s2[1], -4);
+			// 修改容量，与原数组断开联系
+			s2.emplace_back(999, &counter);
+			EXPECT_EQ(s2.size(), 3);
+			EXPECT_GE(s2.capacity(), 3);
+			EXPECT_EQ(s2[1], -4);
+			EXPECT_EQ(s2[2], 999);
+		}
+		EXPECT_EQ(s1[3], -4);
+		EXPECT_EQ(s1[4], V);
+	}
+	EXPECT_EQ(counter, 0);
+	{
+		using std::string;
+		SVector<string> s1 {"1", "abc", "wwwwwwww", "\n\n\t"};
+		SVector<string> s2 = s1.Slice(1, s1.size());
+		EXPECT_EQ(s2[0], "abc");
+		EXPECT_EQ(s2[2], "\n\n\t");
+		s2[1] = "w";
+		EXPECT_EQ(s1[2], "w");
+
+		// 修改容量，两个数组断开联系
+		s1.emplace_back("new data");
+		EXPECT_EQ(s1.size(), 5);
+		EXPECT_EQ(s1[4], "new data");
+		EXPECT_EQ(s2.size(), 3);
+
+		s1[2] = "q";
+		EXPECT_EQ(s2[1], "w");
+		s2[2] = "\n_\n";
+		EXPECT_EQ(s1[3], "\n\n\t");
+	}
+}
+
+// TEST(ResizeTest, T) {
+
+// }
 
 /*
 TODO：
 检查一下数组中的任何对象是否正常构造。用一个特别的构造函数。
 测试功能时要测试多种情况，比如从 vector 构造、从数组指针构造等。
+
+测：s1.reserve; s2=s1; s2.eb; s1再覆盖已有的，不会内存泄露。
 */
